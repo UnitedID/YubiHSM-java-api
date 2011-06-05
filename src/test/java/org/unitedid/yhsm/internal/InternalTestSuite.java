6a1207466a2a5ef843627551e6b64b2a2ae58dfd
Stefan Wold <stefan.wold@unitedid.org>
2011-06-05 21:05:21 +0200

    Added a couple of tests for system info

--- src/test/java/org/unitedid/yhsm/internal/InternalTestSuite.java
+++ src/test/java/org/unitedid/yhsm/internal/InternalTestSuite.java
@@ -22,7 +22,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({EchoCmdTest.class, BufferCmdTest.class, DeviceHandlerTest.class, AEADCmdTest.class, HMACCmdTest.class})
+@Suite.SuiteClasses({EchoCmdTest.class, BufferCmdTest.class, DeviceHandlerTest.class, AEADCmdTest.class, HMACCmdTest.class, SystemInfoCmdTest.class})
 
 public class InternalTestSuite {
 
---
+++ src/test/java/org/unitedid/yhsm/internal/SystemInfoCmdTest.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (c) 2011 United ID. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @author Stefan Wold <stefan.wold@unitedid.org>
+ */
+
+package org.unitedid.yhsm.internal;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.unitedid.yhsm.SetupCommon;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class SystemInfoCmdTest extends SetupCommon {
+
+    @Before
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        super.tearDown();
+    }
+
+    @Test
+    public void testSystemInfo() throws YubiHSMErrorException {
+        Map<String, String> result = hsm.info();
+        List<String> expected = new ArrayList<String>();
+        expected.add("major");
+        expected.add("minor");
+        expected.add("build");
+        expected.add("protocol");
+        expected.add("sysid");
+
+        for (String e : expected) {
+            assertTrue(result.containsKey(e));
+        }
+    }
+
+    @Test
+    public void testSystemInfoToString() throws YubiHSMErrorException {
+        assertEquals("Version ", hsm.infoToString().substring(0, 8));
+    }
+}
